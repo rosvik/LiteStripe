2f24e6533c9961afb14e0b9771192aebf8e2188d
Johannes <j.rosvik@gmail.com>
2019-05-02 23:00:53 +0200

    Update board to byte based communication

--- board/board.ino
+++ board/board.ino
@@ -5,49 +5,20 @@
 PololuLedStrip<11> ledStrip;
 rgb_color colors[LED_COUNT];
 
-long r;
-long g;
-long b;
-
 void setup() {
   Serial.begin(9600);
-  r = 10;
-  g = 10;
-  b = 10;
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String incomingString = Serial.readStringUntil('X');
-    const char* incomingChars = incomingString.c_str();
 
-//    Serial.println("Incoming String: " + incomingString);
+    byte serialdata[432];
 
-    char* pch;
-    // Split string
-    pch = strtok(incomingChars, 'N');
+    int len = Serial.readBytes(serialdata, 432);
 
-    int i = 0;
-    while (pch != NULL) {
-
-      Serial.write(pch);
-
-      long number = strtol(pch, NULL, 16);
-
-      r = number >> 16;
-      g = number >> 8 & 0xFF;
-      b = number & 0xFF;
-      colors[i] = rgb_color(r, g, b);
-
-      Serial.println(String(r) + ", " + String(g) + ", " + String(b));
-      
-      pch = strtok(NULL, 'N');
-      i++;
+    for (int i = 0; i < LED_COUNT; i++) {
+      colors[i] = rgb_color(serialdata[i*3], serialdata[i*3+1], serialdata[i*3+2]);
     }
-
-//    if (incomingString.length() == 6) {
-//      Serial.println(String(number) + ": " + String(r) + ", " + String(b) + ", " + String(g));
-//    }
   
     ledStrip.write(colors, LED_COUNT);
   
