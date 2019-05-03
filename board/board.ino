c3d474e5c0e710d9b8cb2de180b6982166ea23c6
Johannes <j.rosvik@gmail.com>
2019-05-03 16:55:55 +0200

    Remove hard coded values

--- board/board.ino
+++ board/board.ino
@@ -1,27 +1,26 @@
 #include <PololuLedStrip.h>
 #define LED_COUNT 144
+#define LED_PIN 11
+#define BAUDRATE 250000
 
-// Create an ledStrip object and specify the pin it will use.
-PololuLedStrip<11> ledStrip;
+PololuLedStrip<LED_PIN> ledStrip;
 rgb_color colors[LED_COUNT];
 
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(BAUDRATE);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-
-    byte serialdata[432];
-
-    int len = Serial.readBytes(serialdata, 432);
+    byte serialdata[LED_COUNT*3];
+    int len = Serial.readBytes(serialdata, LED_COUNT*3);
 
     for (int i = 0; i < LED_COUNT; i++) {
       colors[i] = rgb_color(serialdata[i*3], serialdata[i*3+1], serialdata[i*3+2]);
     }
-  
+
     ledStrip.write(colors, LED_COUNT);
-  
+
     delay(10);
   }
 }
